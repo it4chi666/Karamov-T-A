3bc123b9b8025ff80dfa62d2ad549c80205e757a
Карамов Т <51236716+it4chi666@users.noreply.github.com>
2022-07-04 08:58:33 +0300

    Update 1.java

--- 1.java
+++ 1.java
@@ -1,6 +1,6 @@
 // Дан массив b (n). Переписать в массив C (n) положительные элементы массива b (n) умноженные на 5
 import java.util.Random;
-public class Focus {
+public class first {
     public static void main(String[] args) {
 
         int m=10;
